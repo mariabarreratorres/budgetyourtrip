2c225e29a8a3c4b826f3185a6bd046ebc0e92a9b
mariabarreratorres <127163154+mariabarreratorres@users.noreply.github.com>
2023-05-15 21:30:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,32 @@ const transport = document.querySelector('#transport');
 const accommodation = document.querySelector('#accommodation');
 const food = document.querySelector('#food');
 const travel = document.querySelector('#travel');
-
 const result = document.querySelector('#result');
+const user = prompt("Hello, ¿What's your name?");
 
-travel.addEventListener('click', function () {
-  const expenses = [parseInt(transport.value), parseInt(accommodation.value), parseInt(food.value)];
+const inputs = Array.from(document.querySelectorAll('input'));
+console.log(inputs);
+function addExpenses(transport, accommodation, food) {
+  return transport + accommodation + food;
+}
 
-  let totalExpenses = 0;
+travel.addEventListener('click', function (e) {
+  e.preventDefault();
 
-  for (let index = 0; index < expenses.length; index++) {
-    totalExpenses = totalExpenses + expenses[index];
-  }
+  // Fields validation
+  if (inputs.some(input => !input.value.trim())) alert('Please fill all the inputs');
 
-  if (totalExpenses > parseInt(budget.value)) {
-    result.innerHTML = 'Lo sentimos, no puedes viajar a ' + destination.value + ' te falta ' + (totalExpenses - parseInt(budget.value));
+  const expenses = addExpenses(+transport.value, +accommodation.value, +food.value);
+
+  // let totalExpenses = 0;
+
+  // for (let index = 0; index < expenses.length; index++) {
+  //   totalExpenses = totalExpenses + expenses[index];
+  // }
+
+  if (expenses > parseInt(budget.value)) {
+    result.innerHTML = user + ' lo sentimos, no puedes viajar a ' + destination.value + ' te falta ' + (expenses - parseInt(budget.value));
   } else {
-    result.innerHTML = 'Felicidades! Puedes viajar a ' + destination.value + ' y te sobra ' + (parseInt(budget.value) - totalExpenses);
+    result.innerHTML = user + ' felicidades! Puedes viajar a ' + destination.value + ' y te sobra ' + (parseInt(budget.value) - expenses);
   }
 });
